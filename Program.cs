4b675ffef3787f6fd7ca55e3cd83a3b989e0d2b6
Mariya Kor <nir21@ya.ru>
2023-11-08 01:53:35 +0300

    first void addaed

--- Program.cs
+++ Program.cs
@@ -3,5 +3,21 @@
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 
-﻿string[] array1 = new string[7] {"first", "1", "text", "2", "3", "home", "theme"};
+string[] array1 = new string[7] { "first", "1", "text", "2", "3", "home", "theme" };
 string[] array2 = new string[array1.Length];
+
+
+
+
+void NewArray(string[] arrayFirst, string[] arraySecond)
+{
+    int count = 0;
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+        if (arrayFirst[i].Length <= 3)
+        {
+            arraySecond[count] = arrayFirst[i];
+            count++;
+        }
+    }
+}
